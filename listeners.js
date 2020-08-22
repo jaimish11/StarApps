637e53993c17e66330e6ecce6f3c05d7ae6e8519
Jaimish Ashar <jaimish97@gmail.com>
2020-08-22 12:24:08 +0530

    Added dynamic enable/disable toggle for add task button.

--- listeners.js
+++ listeners.js
@@ -1,12 +1,27 @@
 //Disable add task button for tasks > 99
 var taskInput = document.getElementById('task-input');
-if (taskInput.value > "99"){
-    document.querySelector('.add-task-btn').disabled = true;
-    document.querySelector('.add-task-btn').className+=" disabled";
+var addTaskBtn = document.querySelector('.add-task-btn');
+if (taskInput.value > 99 || taskInput.value == ""){
+    addTaskBtn.disabled = true;
+    addTaskBtn.className+=" disabled";
 }
 taskInput.addEventListener('keyup',function(){
-    
-    (taskInput.value > "99")?document.querySelector('.add-task-btn').disabled = true:document.querySelector('.add-task-btn').disabled = false;
-    (taskInput.value > "99")?document.querySelector('.add-task-btn').className+=" disabled":document.querySelector('.add-task-btn').classList.remove("disabled");
+    console.log(taskInput.value);
+    console.log(taskInput.value>99);
+    if(taskInput.value==""){
+        console.log('Value null');
+        addTaskBtn.disabled = true;
+        addTaskBtn.className+=" disabled"
+    }
+    else if(taskInput.value != "" && taskInput.value <= 99){
+        console.log('Value less than 99');
+        addTaskBtn.disabled = false;
+        addTaskBtn.classList.remove("disabled");
+    }
+    else if(taskInput.value != "" && taskInput.value > 99){
+        console.log('Value more than 99');
+        addTaskBtn.disabled = true;
+        addTaskBtn.className+=" disabled";
+    }
         
 });
